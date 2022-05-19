790c721a44aaa2095eba363751d5a0c89f970312
srivatsa5 <shrivatsahegde5@gmail.com>
2022-05-19 15:17:30 +0530

    modal completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
-import { Stage, Layer, Text, Circle } from 'react-konva';
-import React from 'react';
-import { ButtonGroup, Button } from '@material-ui/core';
-import ModalComponent from './Components/modal';
-
+import "./App.css";
+import { Stage, Layer, Text, Circle } from "react-konva";
+import React from "react";
+import { ButtonGroup, Button } from "@material-ui/core";
+import ModalComponent from "./Components/modal";
+import WarehouseBorder from "./Models/state_models/outerShapeModels";
+import StorageUnit from "./Models/state_models/skuShapeModel";
 class App extends React.Component {
   constructor() {
     super();
@@ -13,14 +14,15 @@ class App extends React.Component {
       width: 0,
       isModalVisible: false,
       outerWareHouseObj: null,
+      skuUnit: null,
 
       //experimental
-      shapeFormodal: 'rectangle',
+      shapeFormodal: "rectangle"
     };
 
     //write a function which looks whether any stored outerWarehouse object available or not
 
-    window.addEventListener('resize', this.update);
+    window.addEventListener("resize", this.update);
 
     this.toggleModalView = this.toggleModalView.bind(this);
     this.createShapeOnCanvas = this.createShapeOnCanvas.bind(this);
@@ -31,36 +33,42 @@ class App extends React.Component {
   }
 
   componentWillUnmount() {
-    window.removeEventListener('resize', this.update);
+    window.removeEventListener("resize", this.update);
   }
 
   update = () => {
     this.setState({
       height: window.innerHeight,
-      width: window.innerWidth,
+      width: window.innerWidth
     });
   };
 
   //below call back will be passed to button component as well Modal Component to toggle modals view
   toggleModalView(shapeFormodal) {
-    console.log(this, shapeFormodal);
+    console.log(shapeFormodal);
     this.setState({
       isModalVisible: this.state.isModalVisible === true ? false : true,
-      shapeFormodal: shapeFormodal,
+      shapeFormodal: shapeFormodal
     });
   }
 
   //below function will be passed as a callback to Modal component and will add either outerWareHouse object or inner SKU
-  createShapeOnCanvas() {
-    console.log('this callback function creates shape');
-    console.log(this);
-
-    //first check whether outerWarehouse object in this.state is null or not
-    //if null create outerWareHouse Object
-    //else create Storageunit and add it to OuterWareHouse
+  createShapeOnCanvas(props) {
+    console.log("this callback function creates shape");
+    console.log(props.state.length);
+    if (!this.state.outerWareHouseObj) {
+      console.log("outer warehouse not designed");
+      console.log(props.state.shapeType);
+      this.state.outerWareHouseObj = new WarehouseBorder(props.state.shapeType);
+    } else {
+      console.log("warehouse is created already. append this sku to it");
+      console.log(props.state.shapeType);
+      this.skuUnit = new StorageUnit(props.state.shapeType, "biscuit");
+      this.skuUnit.totalVolume = this.skuUnit.getTotalVolumeForStorageUnit();
+    }
 
     this.setState({
-      isModalVisible: this.state.isModalVisible === true ? false : true,
+      isModalVisible: this.state.isModalVisible === true ? false : true
     });
   }
 
@@ -69,40 +77,52 @@ class App extends React.Component {
       <div>
         {this.state.isModalVisible == true ? (
           <ModalComponent
-            callbackFn={this.createShapeOnCanvas}
+            callbackFn={this.createShapeOnCanvas.bind(this)}
             shapeType={this.state.shapeFormodal}
             isModalVisible={this.state.isModalVisible}
           />
         ) : null}
-        <div className='floatleft'>
+        <div className="floatleft">
           <Stage
             width={this.state.height}
             height={this.state.width}
             style={{
-              backgroundColor: 'pink',
-              borderRadius: '15px',
-              overflow: 'hidden',
+              backgroundColor: "pink",
+              borderRadius: "15px",
+              overflow: "hidden"
             }}
           >
             <Layer></Layer>
           </Stage>
         </div>
-        <div className='floatright'>
+        <div className="floatright">
           <div>
             <ButtonGroup
-              variant='contained'
-              aria-label='outlined primary button group'
-              orientation='vertical'
+              variant="contained"
+              aria-label="outlined primary button group"
+              orientation="vertical"
             >
               <Button
                 onClick={(e) => {
-                  this.toggleModalView('square');
+                  this.toggleModalView("square");
                 }}
               >
                 Square
               </Button>
-              <Button>Rectangle</Button>
-              <Button>Circle</Button>
+              <Button
+                onClick={(e) => {
+                  this.toggleModalView("rectangle");
+                }}
+              >
+                Rectangle
+              </Button>
+              <Button
+                onClick={(e) => {
+                  this.toggleModalView("circle");
+                }}
+              >
+                Circle
+              </Button>
             </ButtonGroup>
           </div>
         </div>
--- src/Components/modal.js
+++ src/Components/modal.js
@@ -1,44 +1,143 @@
-import React from 'react';
-import { Modal, Box, Input, Button } from '@material-ui/core';
+import React from "react";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
 
 class ModalComponent extends React.Component {
   constructor(props) {
     super(props);
-    this.length = null;
-    this.width = null;
-    this.radius = null;
-    this.shapeType = props.shapeType;
-    this.callbackFn = props.callbackFn;
-    this.isModalVisible = props.isModalVisible;
+    this.state = {
+      length: "",
+      width: "",
+      radius: "",
+      shapeType: props.shapeType,
+      callbackFn: props.callbackFn,
+      isModalVisible: props.isModalVisible
+    };
 
     this.handleModalSubmit = this.handleModalSubmit.bind(this);
-    console.log(this.shapeType);
+    this.handleChange = this.handleChange.bind(this);
   }
 
   handleModalSubmit(e) {
     e.preventDefault();
+    console.log("clicked submt");
+    console.log(this.state.length);
+    this.state.callbackFn(this);
+  }
 
-    this.callbackFn();
+  handleChange(e) {
+    const { value, name } = e.target;
+    this.setState((prevState) => {
+      if (name == "length") {
+        return {
+          width: prevState.width,
+          radius: prevState.radius,
+          length: value,
+          shapeType: prevState.shapeType,
+          callbackFn: prevState.callbackFn,
+          isModalVisible: prevState.isModalVisible
+        };
+      } else if (name == "width") {
+        return {
+          width: value,
+          radius: prevState.radius,
+          length: prevState.length,
+          shapeType: prevState.shapeType,
+          callbackFn: prevState.callbackFn,
+          isModalVisible: prevState.isModalVisible
+        };
+      } else {
+        return {
+          width: prevState.width,
+          radius: value,
+          length: prevState.length,
+          shapeType: prevState.shapeType,
+          callbackFn: prevState.callbackFn,
+          isModalVisible: prevState.isModalVisible
+        };
+      }
+    });
   }
 
   render() {
-    const ariaLabel = { 'aria-label': 'description' };
-
+    const style = {
+      position: "absolute",
+      top: "40%",
+      left: "50%",
+      transform: "translate(-50%, -50%)",
+      width: 400,
+      bgcolor: "background.paper",
+      border: "3px solid #000",
+      boxShadow: 24,
+      p: 4
+    };
+    const ariaLabel = { "aria-label": "description" };
     return (
       <div>
         <Modal
           open={true}
-          aria-labelledby='modal-modal-title'
-          aria-describedby='modal-modal-description'
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
         >
-          <Box>
-            <Input defaultValue='Hello world' inputProps={ariaLabel} />
-            <Button onClick={this.handleModalSubmit}>Submit</Button>
+          <Box sx={style}>
+            <form onSubmit={this.handleModalSubmit}>
+              <div>
+                {this.state.shapeType === "circle" && (
+                  <div>
+                    <label>
+                      Radius:
+                      <div>
+                        <input
+                          type="text"
+                          name="radius"
+                          value={this.state.radius}
+                          onChange={this.handleChange}
+                        />
+                      </div>
+                    </label>
+                  </div>
+                )}
+                {(this.state.shapeType === "square" ||
+                  this.state.shapeType == "rectangle") && (
+                  <div>
+                    <label>
+                      Length
+                      <div>
+                        <input
+                          type="text"
+                          name="length"
+                          value={this.state.length}
+                          onChange={this.handleChange}
+                        />
+                      </div>
+                    </label>
+                  </div>
+                )}
+                {this.state.shapeType === "rectangle" && (
+                  <div>
+                    <label>
+                      Width:
+                      <div>
+                        <input
+                          type="text"
+                          name="width"
+                          value={this.state.width}
+                          onChange={this.handleChange}
+                        />
+                      </div>
+                    </label>
+                  </div>
+                )}
+              </div>
+              <br></br>
+              <button>Submit</button>
+            </form>
           </Box>
         </Modal>
       </div>
     );
   }
 }
-
 export default ModalComponent;
